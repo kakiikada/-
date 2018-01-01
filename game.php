4124e433998ce9b806679ed7a35035fefb99b340
garchomp-game <otoka.japanesedrum@gmail.com>
2018-01-01 23:06:41 +0900

    作業途中分

---
+++ game.php
@@ -0,0 +1,83 @@
+<?php
+class Meiro{
+  private $branch; //分岐
+  private $sage_root;
+  private $stage = array(1,2,3);
+  private $check_judge;
+  private $count = 0;
+  private $det_flag = 0;
+
+  public function __construct(){
+    if($_POST) {
+      // とある記事を参考にして実装してみました。
+      // postに変更しました。
+      $this->check_judge = $_POST['judge'];
+      $this->count = $_POST['count'];
+      //　それぞれ値を格納したら
+      /*
+      POSTをカラにします。こうすることでリロードした時に
+      ミスが表示されることがなくなります。んで
+      */
+    }
+    if(!(is_null($this->check_judge))){
+      if($this->check_judge == 1){
+        // もしジャッチの結果、１だったら（成功していたら）次の処理をしなさい。
+        $this->count++;
+        // カウントを増やしますよ。
+        $this->det_flag = 0;
+        // フラグは０のままですよ。　　　
+        // ここで、左右の判定で、もし１だったらtrueとして成功へ。２だったら失敗へ振り分け。　下下
+        //　せいこうしたらカウントがプラスされる。３になったらクリア
+      }else{
+        // 違ったら
+        $this->count = 0;
+        if(isset($_SESSION['check_session'], $_POST['check_session']) && $_SESSION['check_session'] === $_POST['check_session']){
+          $this->det_flag = 1;
+        }else{
+          $this->det_flag = 0;
+        }
+        // カウントを０にします。
+        // フラグを立てます。　となると、その下かな。
+        // ココでミスったらdet_flagを１にするとともに、カウントも０にしてる。
+      }
+    }
+    var_dump($this->det_flag);
+    var_dump($_POST['check_session']);
+    var_dump($_SESSION['check_session']);
+  }
+  // count
+  public function getCount(){
+    return $this->count;
+    // 実は簡単に足してるwスタイリングは任せる
+  }
+  public function getDetFlag(){
+    return $this->det_flag;
+    // ココでゲッターの定義をしている。
+    // 返り値を返してあげることで、値を扱えるようにするものですね。
+    // おそらくココの問題もないはず。となれば流行りした…？
+  }
+  // branch
+  public function setBranch($checks){
+    $this->branch = $checks;
+  }
+
+  public function getBranch(){
+    return $this->branch;
+  }
+  // roots
+  public function setRoots($num){
+    $this->stage_root = $num;
+  }
+  public function getRoots(){
+    return $this->stage_root;
+  }
+
+  // stage
+  public function loadStage(){
+
+  }
+  public function delete_count(){
+    $this->count = 0;
+  }
+
+}
